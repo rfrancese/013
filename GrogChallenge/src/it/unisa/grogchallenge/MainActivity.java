12550d74863910f98a1873b4626530fe74d51272
vincenzovenosi <v.venosi@gmail.com>
2014-04-30 16:49:52 +0200

    prova
    Signed-off-by: vincenzovenosi <v.venosi@gmail.com>

--- GrogChallenge/src/it/unisa/grogchallenge/MainActivity.java
+++ GrogChallenge/src/it/unisa/grogchallenge/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		System.out.println("prova");
 		setContentView(R.layout.activity_main);
 	}
 
